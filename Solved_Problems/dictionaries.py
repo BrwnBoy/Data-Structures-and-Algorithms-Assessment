b0b0dc9d5648605ecb92ff5879b13ae37c95c781
Brwni <brianmm665@gmial.com>
2023-08-27 11:27:34 +0300

    Initial commit

---
+++ Solved_Problems/dictionaries.py
@@ -0,0 +1,18 @@
+def word_frequency(sentence):
+    # Remove punctuation and convert to lower case
+    sentence = ''.join(ch for ch in sentence if ch.isalnum() or ch.isspace()).lower()
+    
+    # Split the sentence into words
+    words = sentence.split()
+    
+    # Dictionary to hold the frequency of each word
+    frequency_dict = {}
+    
+    # Count the frequency of each word
+    for word in words:
+        if word in frequency_dict:
+            frequency_dict[word] += 1
+        else:
+            frequency_dict[word] = 1
+    
+    return frequency_dict
